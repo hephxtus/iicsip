ea252d95d4f488caebd7369d95fbc4331fed9566
hephxtus <joe.hodges2000@gmail.com>
2023-03-11 15:43:55 +1300

    mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [isSunny, setIsSunny] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://api.open-meteo.com/v1/forecast?latitude=39.95&longitude=-75.16&hourly=precipitation,weathercode,cloudcover&forecast_days=1"
+      );
+      const data = await response.json();
+      const currentWeatherCode = data.hourly.weathercode[0];
+      if (currentWeatherCode >= 0 && currentWeatherCode <= 2) {
+        setIsSunny(true);
+      } else {
+        setIsSunny(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleClick = () => {
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isSunny === true && <h1>YES</h1>}
+      {isSunny === false && <h1>NO</h1>}
+      {isSunny === null && <h1>Loading...</h1>}
+      <button onClick={handleClick}>Refresh</button>
     </div>
   );
 }
